7b9254cdbd17a22f49bb420060094e45477a04a1
javier ferrari <javierferrarimezher@gmail.com>
2023-04-16 21:54:12 -0300

    login correction google singin

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -64,7 +64,11 @@ function LogIn() {
                         <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                     </div> */}
                     <input type='submit' value='Login to your account' className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'></input>
-                    <div>
+
+                </form>
+                <div className='my-5'>
+
+                <div>
                         <button onClick={() => createUserGoogle()} className='type="button" class="gap-2 text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2'>
                             <img className='w-7 h-7' src={icon} alt='Google'></img>
                             Connect with Google
@@ -74,7 +78,7 @@ function LogIn() {
                     <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                         Not registered? <a href="/signin" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                     </div>
-                </form>
+                </div>
             </div>
   )
 }
